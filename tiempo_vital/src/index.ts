b6da7a5db8d0b00eb857b8f97a991c32e86459e5
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-07 18:42:26 -0600

    index v1

--- tiempo_vital/src/index.ts
+++ tiempo_vital/src/index.ts
@@ -1,9 +1,20 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
+const port = 7000
+//((middleware
+app.use(cors())
+app.use(logger())
+app.use(prettyJSON())
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+//routes
+
+//server
+
+
+
 
 export default app
