f21ecc6a223b46ac2c9ec593613025e52ab1edcd
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-10 11:35:15 -0600

    feat: controller to create user

--- tiempo_vital/src/presentation/controllers/controllers/user.controller.ts
+++ tiempo_vital/src/presentation/controllers/controllers/user.controller.ts
@@ -1,10 +1,20 @@
+import { UserInterface } from "../../../domain/types/user/user.interface";
 import { AppDataSource } from "../../../infrestructure/db/database.connection"
 
 const UserRepository = AppDataSource.getRepository("User");
 export const CreateUser = async (c: any) => {
     try {
-                
+        const user_body: UserInterface = await c.req.json();
+        if (!user_body.name || !user_body.email || !user_body.password || !user_body.rol) {
+            c.status(400);
+            return c.json({ message: "Missing required fields" });
+        }
+        const newUser = UserRepository.create(user_body);
+        await UserRepository.save(newUser);
+        c.status(201);
+        return c.json(newUser);
     } catch (error) {
-        
+        c.status(500);
+        return c.json({ message: "Error creating user", error });
     }
 }
