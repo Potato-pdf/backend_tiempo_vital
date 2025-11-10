bc054e1fb4bbf49504d694743bc34fae99c9129f
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-10 11:29:40 -0600

    feat: user interface

---
+++ tiempo_vital/src/presentation/controllers/controllers/user.controller.ts
@@ -0,0 +1,10 @@
+import { AppDataSource } from "../../../infrestructure/db/database.connection"
+
+const UserRepository = AppDataSource.getRepository("User");
+export const CreateUser = async (c: any) => {
+    try {
+                
+    } catch (error) {
+        
+    }
+}
---
+++ tiempo_vital/src/presentation/routes/user/user.routes.ts
@@ -0,0 +1,8 @@
+import { Hono } from "hono";
+import { CreateUser } from "../../controllers/controllers/user.controller";
+
+
+export const UserRoutes = new Hono();
+
+//Create User
+UserRoutes.post("register", CreateUser);
