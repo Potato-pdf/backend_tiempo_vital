9c30b4b507012d20dd75156185d73b2a99fac0ee
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-10 11:41:05 -0600

    feat. initilize server in port 7000

--- tiempo_vital/src/index.ts
+++ tiempo_vital/src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
 import { prettyJSON } from 'hono/pretty-json'
+import { UserRoutes } from './presentation/routes/user/user.routes'
+import { AppDataSource } from './infrestructure/db/database.connection'
 
 const app = new Hono()
 const port = 7000
@@ -11,8 +13,20 @@ app.use(logger())
 app.use(prettyJSON())
 
 //routes
-
+app.route("/user", UserRoutes);
 //server
+AppDataSource.initialize()
+.then(() => {
+    console.log("Database connected")
+    Bun.serve({
+        fetch: app.fetch,
+        port : port,
+    })
+    console.log(`Server running on port ${port}`)
+})
+.catch((error) => {
+    console.log("Database connection error: ", error)
+})
 
 
 
