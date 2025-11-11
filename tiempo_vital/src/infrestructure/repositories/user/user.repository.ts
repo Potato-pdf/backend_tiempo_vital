0b56ada71ed259842425206fac3a50cc2bb96ad3
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-10 18:36:00 -0600

    user repository(update imports )

---
+++ tiempo_vital/src/infrestructure/repositories/user/user.repository.ts
@@ -0,0 +1,15 @@
+import { Repository } from "typeorm";
+import { User } from "../../../domain/models/users/user.model";
+import { UserRepository } from "../../../domain/repositories/user.repository";
+
+export class TypeORMUserRepository implements UserRepository {
+    constructor(private userRepository: Repository<User>) {}
+
+    async findByEmail(email: string): Promise<User | null> {
+        return await this.userRepository.findOneBy({ email });
+    }
+
+    async save(user: User): Promise<User> {
+        return await this.userRepository.save(user);
+    }
+}
