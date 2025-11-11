b63ad813c7e834d0711d2ded7663b811f01423d4
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-10 20:29:40 -0600

    feat: auth repository

--- tiempo_vital/src/infrestructure/repositories/auth/auth.repository.ts
+++ tiempo_vital/src/infrestructure/repositories/auth/auth.repository.ts
@@ -1,3 +1,18 @@
+import { Repository } from "typeorm";
+import { AuthRepository } from "../../../domain/repositories/auth/auth.repository";
+import { User } from "../../../domain/models/users/user.model";
 
 
-export class TypeORMAuthRepository implements 
+export class TypeORMAuthRepository implements  AuthRepository{
+    constructor(private userRepository: Repository<User>){}
+    
+    async findByEmail(email: string): Promise<any> {
+        return await this.userRepository.findOneBy({ email });
+    }
+    async passwordVerify(password: any): Promise<any> {
+         await Bun.password.verify(this.userRepository.password, this.userRepository.password)
+    }
+    async createToken(user: any): Promise<any> {
+        
+    }
+}
--- tiempo_vital/src/presentation/controllers/auth/auth.controller.ts
+++ tiempo_vital/src/presentation/controllers/auth/auth.controller.ts
@@ -1,2 +1,6 @@
+import { User } from "../../../domain/models/users/user.model";
+import { AppDataSource } from "../../../infrestructure/db/database.connection";
+import { TypeORMAuthRepository } from "../../../infrestructure/repositories/auth/auth.repository";
 
-const AuthRepository = 
+const AuthRepository = new TypeORMAuthRepository(AppDataSource.getRepository(User));
+const createLoginUseCase = new CreateLoginUseCase(AuthRepository)
