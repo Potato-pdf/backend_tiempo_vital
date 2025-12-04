43e7bb2658b18ddc7f6fc58b718929f0672ac8f9
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-04 13:27:57 -0600

    feat: uuid

--- tiempo_vital/src/infrestructure/CQRS/users/user.writting.cqrs.ts
+++ tiempo_vital/src/infrestructure/CQRS/users/user.writting.cqrs.ts
@@ -1,7 +1,7 @@
 import { UserInterfaceCqrs } from "../../../domain/repositories/users/CQRS/user.interface.cqrs";
 import { UsersInterfaceDao } from "../../../domain/repositories/users/DAO/users.interface.dao";
 import { UserInterface } from "../../../domain/types/user/user.interface";
-
+import { randomUUID } from "crypto";
 
 export class UserCQRS implements UserInterfaceCqrs {
     private userDao: UsersInterfaceDao;
@@ -14,15 +14,17 @@ export class UserCQRS implements UserInterfaceCqrs {
         }
         const hashedPassword = await Bun.password.hash(user.password);
         user.password = hashedPassword;
+        user.id = randomUUID();
         return this.userDao.create(user);
     }
     async update(id: string, user: UserInterface): Promise<UserInterface> {
         if (!user.name || !user.email || !user.password || !user.office) {
             throw new Error("Invalid user data");
         }
+        user.id = id;
         return this.userDao.update(id, user);
     }
-    delete(id: string): Promise<void> {
+    async delete(id: string): Promise<void> {
         return this.userDao.delete(id);
-    }
+    }   
 }
