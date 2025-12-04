d3baac1b8e0859d7359881c287867feb95ad2c27
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-04 11:41:53 -0600

    feat: dao interface

---
+++ tiempo_vital/src/domain/interfaces/offices/DAO/office.interface.dao.d.ts
@@ -0,0 +1,10 @@
+import { OfficeInterface } from "../../../types/office/office.interface";
+
+export interface OfficeInterfaceDao {
+    findAll(): Promise<OfficeInterface[]>;
+    findById(id: string): Promise<OfficeInterface | null>;
+    findByName(name: string): Promise<OfficeInterface | null>;
+    create(office: OfficeInterface): Promise<OfficeInterface>;
+    update(id: string, office: OfficeInterface): Promise<OfficeInterface>;
+    delete(id: string): Promise<void>;
+}
