f3256866a4f40baed38c50cf50df11a0d7473578
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-04 15:04:43 -0600

    app svelte

---
+++ frontend/svelte-app/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import "./app.css";
+  import { currentPath } from "./lib/router";
+  import { authStore } from "./stores/authStore";
+  import Login from "./pages/Login.svelte";
+  import Doctores from "./pages/Doctores.svelte";
+  import Administracion from "./pages/Administracion.svelte";
+
+  let path = "/";
+  let isAuthenticated = false;
+
+  currentPath.subscribe((value) => {
+    path = value;
+  });
+
+  authStore.subscribe((state) => {
+    isAuthenticated = state.isAuthenticated;
+  });
+</script>
+
+<main>
+  {#if path === "/"}
+    <Login />
+  {:else if path === "/doctores"}
+    <Doctores />
+  {:else if path === "/administracion"}
+    <Administracion />
+  {:else}
+    <Login />
+  {/if}
+</main>
