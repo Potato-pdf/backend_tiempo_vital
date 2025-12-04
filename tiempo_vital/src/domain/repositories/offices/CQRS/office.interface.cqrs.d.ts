04b90c507670b6d647f92bdfb3ce3e97bcc644bc
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-04 13:25:45 -0600

    fix: office cqrs interface

--- tiempo_vital/src/domain/repositories/offices/CQRS/office.interface.cqrs.d.ts
+++ tiempo_vital/src/domain/repositories/offices/CQRS/office.interface.cqrs.d.ts
@@ -2,9 +2,6 @@ import { OfficeInterface } from "../../../types/office/office.interface";
 
 
 export interface OfficeInterfaceCqrs {
-    findAll(): Promise<OfficeInterface[]>
-    findById(id: string): Promise<OfficeInterface | null>
-    findByName(name: string): Promise<OfficeInterface | null>
     create(office: OfficeInterface): Promise<OfficeInterface>
     update(id: string, office: OfficeInterface): Promise<OfficeInterface>
     delete(id: string): Promise<void>
