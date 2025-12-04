d741218ae03bdd026bdfa39812711668b85309cf
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-04 14:19:49 -0600

    user routes

--- tiempo_vital/src/application/routes/user/user.routes.ts
+++ tiempo_vital/src/application/routes/user/user.routes.ts
@@ -1,8 +1,25 @@
 import { Hono } from "hono";
-import { CreateUser } from "../../../presentation/controllers/user/user.controller";
-
+import {
+    getAllUsersController,
+    getUserByIdController,
+    createUserController,
+    updateUserController,
+    deleteUserController
+} from "../../controllers/user/user.controller";
 
 export const UserRoutes = new Hono();
 
-//Create User
-UserRoutes.post("/register", CreateUser);
+// GET /user - Obtener todos los usuarios
+UserRoutes.get("/", getAllUsersController);
+
+// GET /user/:id - Obtener usuario por ID
+UserRoutes.get("/:id", getUserByIdController);
+
+// POST /user - Crear nuevo usuario
+UserRoutes.post("/", createUserController);
+
+// PUT /user/:id - Actualizar usuario
+UserRoutes.put("/:id", updateUserController);
+
+// DELETE /user/:id - Eliminar usuario
+UserRoutes.delete("/:id", deleteUserController);
