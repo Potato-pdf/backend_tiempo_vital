ce73c2ce0108ceb64e8e04074b86e6b7beaf4a53
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-04 13:25:56 -0600

    fix: user cqrs interface

--- tiempo_vital/src/domain/repositories/users/CQRS/user.interface.cqrs.d.ts
+++ tiempo_vital/src/domain/repositories/users/CQRS/user.interface.cqrs.d.ts
@@ -1,9 +1,6 @@
 import { UserInterface } from "../../../types/user/user.interface";
 
 export interface UserInterfaceCqrs {
-    findAll(): Promise<UserInterface[]>;
-    findById(id: string): Promise<UserInterface | null>;
-    findByName(name: string): Promise<UserInterface | null>;
     create(user: UserInterface): Promise<UserInterface>;
     update(id: string, user: UserInterface): Promise<UserInterface>;
     delete(id: string): Promise<void>;
